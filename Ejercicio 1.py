2d960e7000acf16e74e4c7e43ca2cbab51d58128
Gabriel <gsolaromer@educacion.navarra.es>
2023-11-17 11:34:47 +0100

    He cambiado el print por return

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -3,8 +3,9 @@ def saludar(nombre):
     Saluda al usuario utilizando el nombre proporcionado.
 
     """
-    print(f"¡Hola {nombre}!")
+    return(f"¡Hola {nombre}!")
 # Ejemplo de uso:
 pregunta = input("¿Cómo se llama?")
 nombre_usuario = pregunta
-saludar(nombre_usuario)
+saludo = saludar(nombre_usuario)
+print (saludo)
